4b4bdf15189ddfaaa4f1524cbd9bb4656bf5b2ff
neilsatt <neilsatt@gmail.com>
2018-05-26 11:11:36 -0600

    Page Object section

--- index.js
+++ index.js
@@ -1,55 +1,65 @@
+const fs = require("fs");
 
 const selenium = require("selenium-webdriver");
 const By = selenium.By;
 
+//import file for HomePage file/module
 const HomePage = require('./pages/home');
 
-
 const driver = new selenium.Builder()
-        .forBrowser("chrome")
-        .build();
+    .forBrowser("chrome")
+    .build();
 
+//new instance of HomePage Modile with selenium instance passed in
 const homePage = new HomePage(driver);
+
 homePage.open();
 
-driver.get(process.env.URL);
-
+//list of invitee names
 const invitees = [
-        'Gonzalo Torres del Fierro',
-        'Shadd Anderson',
-        'George Aparece',
-        'Shadab Khan',
-        'Joseph Michael Casey',
-        'Jennifer Nordell',
-        'Faisal Albinali',
-        'Taron Foxworth',
-        'David Riesz',
-        'Maicej Torbus',
-        'Martin Luckett',
-        'Joel Bardsley',
-        'Reuben Varzea',
-        'Ken Alger',
-        'Amrit Pandey',
-        'Rafal Rudzinski',
-        'Brian Lynch',
-        'Lupe Camacho',
-        'Luke Fiji',
-        'Sean Christensen',
-        'Philip Graf',
-        'Mike Norman',
-        'Michael Hulet',
-        'Brent Suggs'
-     ];
+    'Gonzalo Torres del Fierro',
+    'Shadd Anderson',
+    'George Aparece',
+    'Shadab Khan',
+    'Joseph Michael Casey',
+    'Jennifer Nordell',
+    'Faisal Albinali',
+    'Taron Foxworth',
+    'David Riesz',
+    'Maicej Torbus',
+    'Martin Luckett',
+    'Joel Bardsley',
+    'Reuben Varzea',
+    'Ken Alger',
+    'Amrit Pandey',
+    'Rafal Rudzinski',
+    'Brian Lynch',
+    'Lupe Camacho',
+    'Luke Fiji',
+    'Sean Christensen',
+    'Philip Graf',
+    'Mike Norman',
+    'Michael Hulet',
+    'Brent Suggs'
+ ];
 
-
-
-
-
+//store By locators 
 
 invitees.forEach(homePage.addInvitee, homePage);
 
-homePage.removeInvitee('John Fods');
+homePage
+    .findInviteeByName("David Riesz")
+    .remove();
 
+homePage
+    .findInviteeByName("Jennifer Nordell")
+    .toggleConfirmation();
+
+driver.takeScreenshot().then((image, err) => {
+    fs.writeFile("weird-layout.png", image, "base64",
+        err => console.error(err));
+
+});
 
 homePage.toggleNonRespondersVisibility();
 /* 
--- pages/home.js
+++ pages/home.js
@@ -1,33 +1,70 @@
+const By = require("selenium-webdriver").By;
+
 class HomePage {
-    constructor(driver){
+
+    constructor(driver) {
         this.driver = driver;
         this.locators = {
-            inviteeForm: By.id('registrar'),
-            inviteeNameField: By.css('name'),
-            toggleNonRespondersVisibility: By.css('.main > div input'),
-           // removeButtonForInvitee: invitee => By.xpath(`//span[text() ="${invisible}"`)
-    };
+            inviteeForm: By.id("registrar"),
+            //inviteeNameField: By.name("name"),
+            inviteeNameField: By.css("#registrar input[name='name']"),
+            toggleNonRespondersVisibility: By.css(".main > div input"),
+            removeButtonForInvitee: invitee => By.xpath(`//span[text() = "${invitee}"]/../button[last()]`),
+            inviteeByName: name => By.xpath(`//span[text() = "${name}"]/..`)
+        };
     }
 
-    open(){
+    open() {
+
         this.driver.get(process.env.URL);
-    }   
+    }
 
-     addInvitee(name){
-        this.driver.findElement(this.locators.inviteeForm)
-        .sendKeys(name);
+    addInvitee(name) {
+        this.driver.findElement(this.locators.inviteeNameField)
+            .sendKeys(name);
+
         this.driver.findElement(this.locators.inviteeForm).submit();
-    };
+    }
 
-     removeInvitee(invitee){
-            this.driver.findElement(this.locators.removeButtonForInvitee(invitee))
+    toggleNonRespondersVisibility() {
+
+        this.driver.findElement(this.locators.toggleNonRespondersVisibility)
             .click();
     }
 
-     toggleNonRespondersVisibility() {
-            this.driver.findElement(this.locators.toggleNonRespondersVisibility)
-                    .click();
+    findInviteeByName(name) {
+        const el = this.driver
+            .findElement(this.locators.inviteeByName(name));
+
+        return new Invitee(el);
     }
 
+
 }
+
+class Invitee {
+    constructor(element) {
+        this.element = element;
+        this.locators = {
+            removeButton: By.css("button:last-child"),
+            confirmedCheckbox: By.css("input[type='checkbox']")
+
+        };
+
+    }
+
+    remove() {
+        this.element
+            .findElement(this.locators.removeButton)
+            .click();
+    }
+
+    toggleConfirmation() {
+        this.element
+            .findElement(this.locators.confirmedCheckbox)
+            .click();
+
+    }
+}
+
 module.exports = HomePage;
